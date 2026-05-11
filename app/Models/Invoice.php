b610743de0e8c97a419e850704ff9ab45b70a42e
Nehal Khan <nehalkhan4639@gmail.com>
2026-05-11 14:13:07 +0530

    Models and Relationships complete

---
+++ app/Models/Invoice.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+ 
+
+class Invoice extends Model
+{
+   protected $fillable=[
+        'user_id',
+        'client_id',
+        'invoice_numnber',
+        'invoice_date',
+        'due_date',
+        'status',
+        'subtotal',
+        'tax',
+        'discount',
+        'total',
+        'note'
+
+    ];
+
+
+
+
+
+
+
+    public function items(){
+    return $this->hasMany(InvoiceItem::class);
+    }  
+    
+    public function payments(){
+        return $this->hasMany(Payment::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function client(){
+        return $this->belongsTo(Client::class);
+    }
+}
