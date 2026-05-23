cb780adee218f9eb294bbf097d7f6345d42750c0
Nehal Khan <nehalkhan4639@gmail.com>
2026-05-23 14:02:52 +0530

    Fix auth + database setup + seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        \App\Models\User::updateOrCreate(
+            ['email' => 'nehalkhan4639@gmail.com'],
+            [
+                'name' => 'Nehal Khan',
+                'password' => \Illuminate\Support\Facades\Hash::make('4639'),
+            ]
+        );
     }
 }
