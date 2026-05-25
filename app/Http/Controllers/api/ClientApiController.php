77e24d0fffd7e55dda901a93bea4eaca82134911
Nehal Khan <nehalkhan4639@gmail.com>
2026-05-25 13:32:09 +0530

    REST API complete - Invoice and Client endpoints

--- app/Http/Controllers/api/ClientApiController.php
+++ app/Http/Controllers/api/ClientApiController.php
@@ -3,9 +3,39 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ClientApiController extends Controller
 {
-    //
+
+
+    public function index(){
+        $client_api=Client::where('user_id' , auth()->id())->get();
+        return response()->json($client_api);
+
+    }
+
+
+    public function store(Request $request){
+
+        $client_api_store=Client::create([
+
+            'user_id' => $request->auth()->id ,
+            'client_name' => $request->client_name ,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' =>$request->address,
+            'city' => $request->city,
+            'country' => $request->country 
+
+        ]);
+
+        return response()->json([
+            'message ' => 'client data stored successfully' ,
+            'client' =>$client_api_store 
+
+        ] , 201 );
+
+    }
 }
--- app/Http/Controllers/api/InvoiceApiController.php
+++ app/Http/Controllers/api/InvoiceApiController.php
@@ -1,11 +1,50 @@
 <?php
 
-namespace App\Http\Controllers\api;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use APP\Models\Invoice;
 
 class InvoiceApiController extends Controller
 {
-    //
+    public function index()
+    {
+        $invoice_api_data = Invoice::where('user_id', auth()->id())->get();
+        return response()->json($invoice_api_data); //we have to 
+    }
+
+
+    public function show(Invoice $invoice)
+    {
+        return response()->json('invoice');  //here we use route model binding , all data already stored in $invoice varable. 
+    }
+
+    public function store(Request  $request){
+       $invoice= Invoice::create([
+
+            'user_id' => auth()->id(),
+            'client_id' => $request->client_id,
+            'invoice_number' =>'INV -' .str_pad(Invoice::count()+1 , 3 , 0 , STR_PAD_LEFT),
+            
+            'invoice_date' => $request->invoice_date,
+            'due_date' => $request->due_date,
+            'status' => $request->status ?? 'draft',
+            'subtotal' => 0,
+            'tax' =>$request->tax,
+            'discount' =>$request->discount,
+            'total' => 0,
+            'notes' => $request->notes
+
+        ]);
+
+        return response( ) -> json([
+            'message' => 'Invoice created successfully',
+            'invoice' => $invoice
+        ] , 201 ) ;
+         
+
+
+    }
+ 
 }
