cf42903997b7083642ef743ecff60a77ecac0a49
Nehal Khan <nehalkhan4639@gmail.com>
2026-05-26 21:41:21 +0530

    Fix Invoice model namespace

--- app/Http/Controllers/Api/InvoiceApiController.php
+++ app/Http/Controllers/Api/InvoiceApiController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use APP\Models\Invoice;
+use App\Models\Invoice;
 
 class InvoiceApiController extends Controller
 {
